b11f2c413384fdb3d999159c0205c9daddba57eb
grimir <falta.vit@zsbcuo.cz>
2020-09-20 20:19:16 +0200

    fixed school view

--- resources/views/livewire/show-registration.blade.php
+++ resources/views/livewire/show-registration.blade.php
@@ -28,10 +28,10 @@
                     ];
                 }
             @endphp
-            <div class="h-full flex flex-col sm:flex-row justify-around items-center mt-10 sm:mt-32 gap-y-6">
+            <div class="h-full flex flex-col sm:flex-row justify-around items-center sm:mt-32">
                 @foreach($links as $link)
                     <div
-                        class="flex flex-col align-center text-center bg-gray-300 sm:px-32 sm:py-16 px-16 py-8 rounded w-3/4 sm:w-auto">
+                        class="mt-10 flex flex-col align-center text-center bg-gray-300 sm:px-32 sm:py-16 px-16 py-8 rounded w-3/4 sm:w-auto">
                         <h1 class="text-2xl">{{$link['title']}}</h1>
                         <a href="{{$link['link']}}"
                            class="hover:text-bold self-center text-white bg-blue-700 py-1 px-2 sm:py-2 sm:px-4 sm:w-36 w-full mt-5 block">
