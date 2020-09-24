3714839f1741dc33e02c170f72d30f9ea7ba18f3
Matěj Půhoný <info@jetam.eu>
2020-09-24 22:56:34 +0200

    School detail

--- resources/views/livewire/show-school.blade.php
+++ resources/views/livewire/show-school.blade.php
@@ -5,66 +5,123 @@
 
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 w-100 px-4">
-            <h2 class="font-semibold text-2xl text-gray-800 leading-tight mb-8">{{$school->name}}</h2>
-            <div>
-                <h2 class="text-2xl">Informace</h2>
-                <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 text-center sm:text-left">
-                    <div>
-                        <a href="http://maps.google.com/?q={{$school->address}}, {{$school->psc}} {{$school->city}}" target="_blank" class="font-bold">
-                            {{$school->address}}<br/>
-                            {{$school->psc}} {{$school->city}}
-                        </a>
-                        <div class="mt-4"></div>
-                        <a href="mailto:{{$school->email}}" class="font-bold">{{$school->email}}</a><br/>
-                        <a href="tel:{{$school->phone}}" class="font-bold">{{$school->phone}}</a><br/>
-                        <a href="{{fix_url($school->web)}}" class="font-bold">{{$school->web}}</a><br/>
+            <h1 class="text-3xl text-gray-800 leading-tight mb-8 font-light">
+                {{$school->name}}
+            </h1>
+
+            <div class="grid md:grid-cols-2 gap-3">
+                <div class="bg-white p-5 shadow-sm box-border">
+                    {!! $school->description !!}
+                </div>
+                <div>
+                    <div class="bg-white p-5 shadow-sm box-border">
+                        <h2 class="p-2">Informace</h2>
+                        <dl>
+                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-sm leading-5 font-medium text-gray-500">
+                                    Adresa
+                                </dt>
+                                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                    <a href="http://maps.google.com/?q={{$school->address}}, {{$school->psc}} {{$school->city}}" target="_blank">
+                                        {{$school->address}}<br/>
+                                        {{$school->psc}} {{$school->city}}
+                                    </a>
+                                </dd>
+                            </div>
+                            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-sm leading-5 font-medium text-gray-500">
+                                    Email
+                                </dt>
+                                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                    <a href="mailto:{{$school->email}}">{{$school->email}}</a>
+                                </dd>
+                            </div>
+                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-sm leading-5 font-medium text-gray-500">
+                                    Telefon
+                                </dt>
+                                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                    <a href="tel:{{$school->phone}}">{{$school->phone}}</a>
+                                </dd>
+                            </div>
+                            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-sm leading-5 font-medium text-gray-500">
+                                    Webové stránky
+                                </dt>
+                                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                    <a href="{{fix_url($school->web)}}">{{$school->web}}</a>
+                                </dd>
+                            </div>
+                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt class="text-sm leading-5 font-medium text-gray-500">
+                                    Přílohy
+                                </dt>
+                                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                    <ul class="border border-gray-200 rounded-md">
+                                        <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm leading-5">
+                                            <div class="w-0 flex-1 flex items-center">
+                                                <!-- Heroicon name: paper-clip -->
+                                                <svg class="flex-shrink-0 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                                                </svg>
+                                                <span class="ml-2 flex-1 w-0 truncate">
+                                                    Informační brožura.pdf
+                                                </span>
+                                            </div>
+                                            <div class="ml-4 flex-shrink-0">
+                                                <a href="https://deltassie-my.sharepoint.com/:b:/g/personal/formji_delta-skola_cz/EZay0T1NZ25Kp-7G4A3oPFoBheEckjImbPqoHEw6zVh7pw?e=DK63nk" class="font-medium text-indigo-600 hover:text-indigo-500 transition duration-150 ease-in-out" target="_blank">
+                                                    Stáhnout
+                                                </a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </dd>
+                            </div>
+                        </dl>
                     </div>
-                    <div class="mt-4 sm:mt-0">
-                        <span><a href="https://deltassie-my.sharepoint.com/:b:/g/personal/formji_delta-skola_cz/EZay0T1NZ25Kp-7G4A3oPFoBheEckjImbPqoHEw6zVh7pw?e=DK63nk" target="_blank" class="font-bold"><img src="{{asset("/images/pdf.svg")}}" alt="PDF"
-                                                                 class="w-6 h-6 inline-block mr-4">Informační brožura.pdf</a></span>
+                    <div class="bg-white p-5 shadow-sm box-border mt-3">
+                        <h2 class="p-2">Obory</h2>
+                        @foreach ($school->specializations as $specialization)
+                            <div class="{{ $loop->index % 2 === 0 ? "bg-gray-50": "bg-white"}} px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
+                                <div class="text-sm leading-5 font-medium text-gray-500">
+                                    {{$specialization->prescribed_specialization->code}} - {{$specialization->prescribed_specialization->name}} <br>
+                                    (ŠVP: <i>{{$specialization->name}}</i>)
+                                </div>
+                                <div class="mt-5 text-sm leading-5 text-gray-900 sm:mt-3">
+                                    <a class="btn btn-primary" href="/obor/{{$specialization->id}}">Více informací o oboru</a>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
-            <div class="mt-8 grid school-detail-grid">
-                <div>
-                    <h2 class="text-2xl">Obory</h2>
-                    <ul class="flex flex-col gap-row-2">
-                        @foreach($school->specializations as $specialization)
-                            <li class="list-disc py-1 ml-8">
-                                <a href="/obor/{{$specialization->id}}">{{$specialization->prescribed_specialization->code}}
-                                    - {{$specialization->prescribed_specialization->name}}</a><br/>
-                                (ŠVP: <i>{{$specialization->name}}</i>)
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="text-2xl">Výstavy</h2>
-                    <ul class="flex flex-col gap-row-2">
-                        @foreach($school->ordered_registrations()->get() as $registration)
-                            <li class="grid grid-cols-1 xl:grid-cols-2 ml-8 py-2">
-                                <div>
-                                    <a class="list-disc-block"
-                                       href="/vystava/{{$registration->exhibition->id}}">{{format_date($registration->exhibition->date)}} {{$registration->exhibition->district->name}}
-                                        ({{$registration->exhibition->name}})</a><br/>
+
+            <div class="bg-white p-5 shadow-sm box-border mt-3">
+                <h2 class="text-2xl">Výstavy</h2>
+                @foreach ($school->registrations as $registration)
+                    <div class="{{ $loop->index % 2 === 0 ? "bg-gray-50": "bg-white"}} px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
+                        <div class="text-sm leading-5 font-medium text-gray-500">
+                            <a class=""
+                               href="/vystava/{{$registration->exhibition->id}}">
+                                <div class="">
+                                    {{$registration->exhibition->district->region->name}}
+                                    {{format_date($registration->exhibition->date)}}
                                 </div>
-                                <div class="mt-5 xl:mt-0">
-                                    {{--                                TODO: variable hours--}}
-                                    {{--                                zobrazit jen pokud se kona dnes--}}
-                                    <span class="flex flex-col xl:ml-8 sm:inline-block sm:text-right">
-                                        <a href="/vstoupit/ranni/{{$registration->id}}" target="_blank"
-                                           class="btn text-sm text-center btn-primary">Online {{settings("morning_event_start")}} - {{settings("morning_event_end")}}</a>
-                                        <a href="/vstoupit/vecerni/{{$registration->id}}" target="_blank"
-                                           class="mt-4 text-sm sm:mt-0 sm:ml-2 text-center btn btn-primary">Online {{settings("evening_event_start")}} - {{settings("evening_event_end")}}</a>
-                                    </span>
+                                <div class="text-lg font-light">
+                                    <span class="font-black">{{$registration->exhibition->district->name}}</span> ({{$registration->exhibition->name}})
                                 </div>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-            <div class="mt-16">
-                {!! $school->description !!}
+                            </a>
+                        </div>
+                        <div class="mt-5 text-sm leading-5 text-gray-900 sm:mt-3">
+                            {{--                                TODO: variable hours--}}
+                            {{--                                zobrazit jen pokud se kona dnes--}}
+                            <a href="/vstoupit/ranni/{{$registration->id}}" target="_blank"
+                               class="btn text-sm text-center btn-primary">Online {{settings("morning_event_start")}} - {{settings("morning_event_end")}}</a>
+                            <a href="/vstoupit/vecerni/{{$registration->id}}" target="_blank"
+                               class="mt-4 text-sm sm:mt-0 sm:ml-2 text-center btn btn-primary">Online {{settings("evening_event_start")}} - {{settings("evening_event_end")}}</a>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
