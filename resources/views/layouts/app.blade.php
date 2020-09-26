6e7be5bf3ecf2bb6a99a55e2c205537293587bc2
grimir <falta.vit@zsbcuo.cz>
2020-09-26 11:59:21 +0200

    defer loading of js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
 
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.2.1/dist/alpine.js" defer></script>
-        <script src="{{asset("js/app.js")}}"></script>
+        <script src="{{asset("js/app.js")}}" defer></script>
     </head>
     <body>
         <div class="min-h-screen bg-gray-100">
