1fbe365783521a528b3848d4024db889c5d573fb
Matěj Půhoný <info@jetam.eu>
2020-09-26 19:13:31 +0200

    Orders redesign

--- resources/views/livewire/create-order.blade.php
+++ resources/views/livewire/create-order.blade.php
@@ -1,63 +1,85 @@
 <div>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Nová objednávka
-        </h2>
-    </x-slot>
+    <x-own-header>
+        Nová objednávka
+    </x-own-header>
 
     <div class="py-12">
         <x-dashboard-card>
             <div>
-                @if(count($selected_exhibitions) > 0)
-                    <table class="table-auto w-full">
+                <table class="table-auto w-full table divide-y divide-gray-200">
+                    <thead>
                         <tr>
-                            <th class="px-4 py-2">Výstava</th>
-                            <th class="px-4 py-2">Cena</th>
-                            <th class="px-4 py-2"></th>
-                            <th class="px-4 py-2"></th>
+                            <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                Výstava
+                            </th>
+                            <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                Cena
+                            </th>
+                            <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+
+                            </th>
+                            <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+
+                            </th>
                         </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200">
                         @foreach($selected_exhibitions as $se)
-                            <tr>
-                                <td class="border px-4 py-2">{{format_date($se['date'])}} - {{$se['city']}}
+                            <tr class="{{$loop->index %2 == 1 ? "" : ""}}">
+                                <td class="px-4 py-2">{{format_date($se['date'])}} - {{$se['city']}}
                                     ({{$se['name']}})
                                 </td>
-                                <td class="border px-4 py-2">
+                                <td class="px-4 py-2">
                                     @if($is_first_order)
-                                        0 kč
+                                        {{number_format(0, 0,",",".")}},- Kč
                                         @php
                                             $is_first_order = false;
                                         @endphp
                                     @else
-                                        1000 kč
+                                        {{number_format(1000,0,",",".")}},- Kč
                                     @endif
                                 </td>
-                                <td class="border px-4 py-2 text-center">
+                                <td class="px-4 py-2 text-center">
                                     <button wire:click="edit({{$se['id']}})" class="link">Upravit</button>
                                 </td>
-                                <td class="border px-4 py-2 text-center">
+                                <td class="px-4 py-2 text-center">
                                     <button wire:click="remove({{$se['id']}})" class="link">Odstranit</button>
                                 </td>
                             </tr>
                         @endforeach
-                        <tr>
-                            <td></td>
+
+                        @if(count($selected_exhibitions) < 1)
+                            <tr>
+                                <td class="w-1/2"></td>
+                                <td colspan="3" class="text-center pb-4">
+
+                                </td>
+                            </tr>
+                        @endif
+                    </tbody>
+                    <tfoot class="">
+                        <tr class="mt-5 border-double border-t-4 border-gray-200">
+                            <td class="px-4 py-2">Celkem</td>
                             <td class="px-4 py-2">
-                                @if($price == 0)
-                                    Zdarma
-                                @else
-                                    {{$price}},- Kč
-                                @endif
+                                {{number_format($price,0,",",".")}},- Kč
                             </td>
-                            <td class="2"></td>
+                            <td></td>
+                            <td class="text-right">
+                                <button wire:click="add" class="btn btn-primary mt-4 text-sm">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline-block h-5 mr-2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                    <div class="align-middle inline-block">Přidat výstavu</div>
+                                </button>
+                            </td>
                         </tr>
-                    </table>
-                @endif
-                <button wire:click="add" class="link mt-4">Přidat výstavu</button>
+                    </tfoot>
+                </table>
             </div>
 
-            <div class="flex mt-6">
-                <button wire:click="cancel" class="link">Zrušit</button>
-                <button id="complete" class="link ml-4">Dokončit</button>
+            <div class="mt-6 text-right mt-20">
+                <button wire:click="cancel" class="text-gray-400">Zrušit</button>
+                <button id="complete" class="btn bg-green-400 text-white ml-4">Dokončit</button>
             </div>
         </x-dashboard-card>
     </div>
