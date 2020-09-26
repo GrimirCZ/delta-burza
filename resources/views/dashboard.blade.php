c3b4a1f7428edec690d63cd76d3167b35b7501e9
Matěj Půhoný <info@jetam.eu>
2020-09-26 13:38:03 +0200

    WTF lost changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,17 @@
         </h2>
     </x-slot>
 
+    <div class="sm:flex justify-between max-w-7xl mx-auto py-10 pb-0 px-2 sm:px-6 lg:px-8 w-100 items-center">
+        <div class="inline-block">
+            <div class="top text-gray-600">{{Auth::user()->school->district->name}}</div>
+            <h1 class="font-light text-3xl text-gray-800">{{Auth::user()->school->name}}</h1>
+        </div>
+        <div class="inline-block">
+            <img src="{{asset('storage/' . Auth::user()->school->logo())}}" class="h-8" alt="Logo {{Auth::user()->school->name}}">
+        </div>
+    </div>
+
+
     <div class="py-12">
         <x-dashboard-card>
             @if(Auth::user()->school_id == null)
