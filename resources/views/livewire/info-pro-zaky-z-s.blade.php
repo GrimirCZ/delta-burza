9c7800111b864cebb23e364c696e3b7fb444a87c
Matěj Půhoný <info@jetam.eu>
2020-09-29 08:50:47 +0200

    Fix texts

--- resources/views/livewire/info-pro-zaky-z-s.blade.php
+++ resources/views/livewire/info-pro-zaky-z-s.blade.php
@@ -5,47 +5,59 @@
 
     <div class="py-12">
         <x-dashboard-card>
-            <p>Tak to vypadá, že kvůli COVIDu budou letos prezenční výstavy středních škol
+            <p>
+                Tak to vypadá, že kvůli COVIDu budou letos prezenční výstavy středních škol
                 asi zrušené. Chceme vám pomoci s výběrem střední
                 školy aspoň pomocí technologií a výstavy udělat online. Velká většina z vás
                 zažila letos na jaře nějakou formu on-line výuky ať už pomocí Microsoft Teamsů,
                 Google Sheetsů nebo třeba technologií Zoom. Pojďte si pomocí stejných
                 technologií prohlédnout své budoucí školy a on-line si popovídat s jejich
-                zástupci.</p>
-            <h2 class="header">Jak na to?</h2>
-            <h3 class="subheader">Výběr podle místa výstavy a termínu</h3>
-            <p>Nejjednodušší volba, která nejvíce odpovídá klasickým výstavám. Nejdříve si
-                zkrátka vyberete "výstavu", na kterou byste chtěli "jít". Například, když chcete
-                studovat na nějaké škole z Pardubic a okolí, vyberete si prostě výstavu
-                "Pardubice (Schola Bohemia)", podíváte se, kdy se koná - např.: 6.11.2020 a už
-                dopředu si můžete prohlédnout školy (a jejich obory), které se výstavy
-                zúčastní.</p>
-            <p>6.11.2020 potom přijdete k počítači, rozkliknete si výstavu "v Pardubicích" a
-                u škol, které vás zaujaly, se postupně připojíte do jejich on-line schůzek.
-                BurzaŠkol.Online nabízí vždy připojení dopoledne 8:00-12:00, kde můžete se
-                středními školami diskutovat ze své základky a potom ten samý den večer
-                18:00-21:00, kdy můžete vše ještě jednou probrat i se svými rodiči v klidu z
-                domova.</p>
-            <h3 class="subheader">Filtr podle vysněného oboru</h3>
-            <p>Jelikož jste na internetovém tržišti a všechna data máme hezky pohromadě, tak
-                nám BurzaŠkol.Online umožňuje i to, co normální prezenční výstava neumí. A to
-                například vyfiltrovat pouze ty školy, kde se učí náš vysněný obor. Když
-                například budu chci jít studovat informační technologie v Pardubickém,
-                Královéhradeckém nebo Středočeském kraji, budu postupovat následovně:</p>
-            <ul class="marked-list">
-                <li>Stisknu tlačítko "Filtrovat školy"</li>
-                <li>Zvolím obor, který chci studovat např.: 18-20-M/01 - Informační technologie (případně skupinu
-                    oborů).
-                </li>
+                zástupci.
+            </p>
+
+            <h2 class="text-xl text-black pt-5 pb-2">Jak na to?</h2>
+            <h3 class="text-lg text-black pt-5 pb-2">Výběr podle místa výstavy a termínu</h3>
+            <p>
+                Nejjednodušší volba, která nejvíce odpovídá klasickým výstavám. Vyberte si kraj a potom zvolte výstavu, na kterou byste chtěli "jít".
+                Například, když chcete studovat na nějaké škole z Pardubic a okolí, vyberete si prostě výstavu "Pardubice (Schola Bohemia)", podíváte se,
+                kdy se koná - např.: 6.11.2020 a už dopředu si můžete prohlédnout školy (a jejich obory), které se výstavy zúčastní.
+            </p>
+            <p class="mt-4">
+                6. 11. 2020 potom přijdete k počítači, rozkliknete si výstavu "v Pardubicích" a u škol, které vás zaujaly, se postupně připojíte
+                do jejich on-line schůzek. BurzaŠkol.Online nabízí vždy připojení dopoledne 8:00-12:00, kde můžete se středními školami diskutovat
+                ze své základky a potom ten samý den večer 18:00-21:00, kdy můžete vše ještě jednou probrat i se svými rodiči v klidu z domova.
+            </p>
+
+
+            <h3 class="text-xl text-black pt-10 pb-2">Filtr podle vysněného oboru</h3>
+
+            <p class="">
+                Jelikož jste na internetovém tržišti a všechna data máme hezky pohromadě, tak nám BurzaŠkol.Online umožňuje i to,
+                co normální prezenční výstava neumí. A to například vyfiltrovat pouze ty školy, kde se učí náš vysněný obor.
+                Když například budu chtít jít studovat informační technologie v Pardubickém, Královéhradeckém nebo Středočeském kraji,
+                budu postupovat následovně:
+            </p>
+
+            <ul class="marked-list mt-5">
+                <li>Stisknu tlačítko s trychtýřkem "Filtrovat školy",</li>
+                <li>zvolím typ studia „Střední odborné školy (s maturitou),</li>
+                <li>zvolím zaměření „SOŠ technické a IT“,</li>
+                <li>a nakonec vyberu obor, který chci studovat např.: 18-20-M/01 - Informační technologie (případně skupinu oborů).</li>
                 <li>Zaškrtnu kraje, které jsou pro mne přijatelné z hlediska dojezdové vzdálenosti.</li>
-                <li>A nakonec stisknu tlačítko "Zobraz školy".</li>
+                <li>A nakonec stisknu tlačítko "Zobrazit školy".</li>
             </ul>
-            <p>Po jeho stisknutí se zobrazí všechny školy ze zvolených regionů, kde se vyučuje
-                zvolený obor. Přímo u škol se zároveň zobrazí termíny a názvy výstav, kterých se škola účastní.</p>
-            <p>Pod školami se zobrazí ještě termíny výstav seřazených dle počtu škol, které vyhovují vašim
-                podmínkám.</p>
-            <p>Můžete si tak vybrat termíny, kdy si na BurzaŠkol.Online přijdete s vybranými školami on-line
-                popovídat.</p>
+
+
+            <p class="mt-5">
+                Po jeho stisknutí se zobrazí všechny školy ze zvolených regionů, kde se vyučuje zvolený obor.
+                Přímo u škol se zároveň zobrazí termíny a názvy výstav, kterých se škola účastní.
+            </p>
+            <p class="mt-5">
+                Pod školami se zobrazí ještě termíny výstav seřazených dle počtu škol, které vyhovují vašim podmínkám.
+            </p>
+            <p class="mt-5">
+                Můžete si tak vybrat termíny, kdy si na BurzaŠkol.Online přijdete s vybranými školami on-line popovídat.
+            </p>
         </x-dashboard-card>
     </div>
 </div>
