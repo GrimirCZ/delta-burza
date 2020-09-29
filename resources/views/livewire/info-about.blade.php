1cc6804eda0782aceccb39b4ae4dd00ec9fb39da
Matěj Půhoný <info@jetam.eu>
2020-09-29 08:23:20 +0200

    About us page edit

--- resources/views/livewire/info-about.blade.php
+++ resources/views/livewire/info-about.blade.php
@@ -6,16 +6,13 @@
 
     <div class="mt-5 mx-3">
         <x-dashboard-card>
-            <h2 class="header">Studentský projekt 6 dní a 6 nocí</h2>
-            <p>
-                A neb jak dvojice šílených studentů, bláznivý učitel počítačové grafiky a praštěný ředitel během šesti dnů a nocí připravili portál pro volbu studia
+            <h2 class="text-xl text-black pt-5 pb-2">
+                Studentský projekt 6 dní a 6 nocí. A neb jak dvojice šílených studentů, bláznivý učitel počítačové grafiky a praštěný ředitel během šesti dnů a nocí připravili portál pro volbu studia
                 pro deváťáky v době Koronaviru pro ČR.
-            </p>
+            </h2>
 
-            <div class="mt-5"></div>
-
-            <h3 class="subheader">Portál BurzaŠkol.Online vznikl jako studentský projekt na Střední škole informatiky a ekonomie DELTA v Pardubicích.</h3>
-            <p>
+            <p class="mt-10 font-bold">Portál BurzaŠkol.Online vznikl jako studentský projekt na Střední škole informatiky a ekonomie DELTA v Pardubicích.</p>
+            <p class="mt-8">
                 Vznikal ve 4 lidech. Vznikal šest dní a šest nocí v termínu 22.-28.9. Proč tak narychlo? Protože dřív nás to nenapadlo a první výstavy začínají 1.10.!
                 Předem se omlouváme, pokud najdete nějaké překlepy, technické nedokonalosti atd. Prosíme, sdělte nám je, nejlépe e-mailem a my se je pokusíme co nejdříve opravit.
                 Nejsme firma, jsme škola. Pouze jsme se pokusili vyřešit akutní problém, před kterým všechny střední školy a učiliště a všechny současní deváťáci stojí.
@@ -25,7 +22,7 @@
 
             <div class="mt-5"></div>
 
-            <p>
+            <p class="mt-10">
                 <b>Vít Falta</b> - 4. ročník (programátor backend části), <br>
                 <b>Matěj Půhoný</b> - 4. ročník (programátor frontend části),<br>
                 <b>akademický malíř Daniel Václavík</b> - učitel počítčové grafiky (grafický návrh),<br>
@@ -34,8 +31,8 @@
 
             <div class="mt-5"></div>
 
-            <h3 class="subheader">Kontakty & technická podpora</h3>
-            <p>
+            <h3 class="text-xl text-black pt-5 pb-2">Kontakty & technická podpora</h3>
+            <p class="">
                 Email: <a href="mailto:jiri.formanek@delta-skola.cz" class="link">jiri.formanek@delta-skola.cz</a> (v předmětu, prosím, úvádějte "BurzaŠkol.Online") <br>
                 Telefon: <a href="tel:466611106" class="link">466 611 106</a><br>
                 On-line schůzka: <a href="https://bit.ly/36eksH3" class="link">https://bit.ly/36eksH3</a> <br>
