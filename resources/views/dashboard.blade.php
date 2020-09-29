713ae721c1e6dc742231197da8c5f29ce4f1d022
Matěj Půhoný <info@jetam.eu>
2020-09-29 11:22:50 +0200

    add view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -38,6 +38,12 @@
                             </svg>
                             <span class="inline-block align-middle">Upravit</span>
                         </a>
+                        <a href="{{url("/skola/".Auth::user()->school->id)}}" class="text-header ml-4 inline-block">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline h-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                            </svg>
+                            <span class="inline-block align-middle">Zobrazit datail školy</span>
+                        </a>
                     </h3>
                 </div>
                 <div>
