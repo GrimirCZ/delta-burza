ff45cff3aaaf9757ab6af9d36eb1fa17e5139750
grimir <falta.vit@zsbcuo.cz>
2020-10-01 18:11:08 +0200

    correct grammatical mistakes

--- resources/views/livewire/show-exhibition.blade.php
+++ resources/views/livewire/show-exhibition.blade.php
@@ -15,7 +15,7 @@
                                   d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"/>
                         </svg>
                         <h3 class="ml-3 inline-block text-3xl align-middle">Do této výstavy se zatím žádný vystavovatel
-                            nepřihlásil. Buďte být první!</h3>
+                            nepřihlásil. Buďte první!</h3>
                     </div>
                 @else
                     <div class="grid md:grid-cols-2 gap-3">
