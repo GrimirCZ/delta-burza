bb6b0a3d4c5807aa33719967b00cbbb5f6b4bdf2
grimir <falta.vit@zsbcuo.cz>
2020-10-01 18:10:22 +0200

    correct grammatical mistakes

--- resources/views/livewire/show-filtered-schools.blade.php
+++ resources/views/livewire/show-filtered-schools.blade.php
@@ -30,7 +30,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                           d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                 </svg>
-                <h3 class="ml-3 inline-block text-3xl align-middle">Nebyli nalezeni žádní vystavovatelé, kteří by odpovídali podmínkám</h3>
+                <h3 class="ml-3 inline-block text-3xl align-middle">Nebyli nalezeni žádní vystavovatelé, kteří by odpovídali podmínkám.</h3>
 
                 <div class="align-center">
                     <button
@@ -119,7 +119,7 @@
                           d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                 </svg>
                 <h3 class="ml-3 inline-block text-3xl align-middle">Nebyly nalezeny žádné výstavy s vystavovateli, kteří
-                    by odpovídali podmínkám</h3>
+                    by odpovídali podmínkám.</h3>
             </div>
         @endif
         <div class="grid md:grid-cols-2 gap-3">
