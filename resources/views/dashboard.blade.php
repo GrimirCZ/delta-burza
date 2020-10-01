da0d18eb1a9717d1d71c975f93dc07f4e7bd44b9
Matěj Půhoný <info@jetam.eu>
2020-10-01 18:24:47 +0200

    show specs only to schools

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -134,61 +134,63 @@
                 </div>
             </div>
 
-            <div class="bg-white shadow overflow-hidden sm:rounded-lg mx-5 mt-10">
-                <div class="px-4 py-5 border-b border-gray-200 sm:px-6 flex justify-between">
-                    <h3 class="text-lg leading-6 font-medium text-gray-900">
-                        Obory
-                        <a href="{{url("/obor/vytvorit")}}" class="text-header ml-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor" class="inline h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                            </svg>
-                            <span class="inline-block align-middle">Přidat obor</span>
-                        </a>
-                    </h3>
-                </div>
-                <div>
-                    <table class="table-fixed min-w-full text-gray-500">
-                        <tbody>
-                        @foreach(Auth::User()->school->ordered_specializations()->get() as $specialization)
-                            <tr class="{{$loop->index %2 == 0 ? "bg-gray-100" : ""}}">
-                                <td class="px-8 py-5">
-                                    {{$specialization->prescribed_specialization->code}}
-                                    - {{$specialization->prescribed_specialization->name}}<br/>
-                                    (ŠVP: <i>{{$specialization->name}})</i>
-                                </td>
-                                <td class="px-8 py-5">{{substr(strip_tags($specialization->description), 0, 50)."..."}}</td>
-                                <td class="px-8 py-5 text-right">
-                                    <a href="/obor/{{$specialization->id}}/upravit" class="text-header ml-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                             stroke="currentColor" class="inline h-5">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
-                                        </svg>
-                                        <span class="inline-block align-middle">Upravit</span>
-                                    </a>
-                                    <form action="/obor/{{$specialization->id}}/smazat" class="inline ml-4"
-                                          method="post">
-                                        @csrf
-                                        @method("DELETE")
-                                        <button type="submit">
+            @if(Auth::User()->school->is_school)
+                <div class="bg-white shadow overflow-hidden sm:rounded-lg mx-5 mt-10">
+                    <div class="px-4 py-5 border-b border-gray-200 sm:px-6 flex justify-between">
+                        <h3 class="text-lg leading-6 font-medium text-gray-900">
+                            Obory
+                            <a href="{{url("/obor/vytvorit")}}" class="text-header ml-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke="currentColor" class="inline h-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                </svg>
+                                <span class="inline-block align-middle">Přidat obor</span>
+                            </a>
+                        </h3>
+                    </div>
+                    <div>
+                        <table class="table-fixed min-w-full text-gray-500">
+                            <tbody>
+                            @foreach(Auth::User()->school->ordered_specializations()->get() as $specialization)
+                                <tr class="{{$loop->index %2 == 0 ? "bg-gray-100" : ""}}">
+                                    <td class="px-8 py-5">
+                                        {{$specialization->prescribed_specialization->code}}
+                                        - {{$specialization->prescribed_specialization->name}}<br/>
+                                        (ŠVP: <i>{{$specialization->name}})</i>
+                                    </td>
+                                    <td class="px-8 py-5">{{substr(strip_tags($specialization->description), 0, 50)."..."}}</td>
+                                    <td class="px-8 py-5 text-right">
+                                        <a href="/obor/{{$specialization->id}}/upravit" class="text-header ml-4">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                  stroke="currentColor" class="inline h-5">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                                                      d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
                                             </svg>
-                                            <span class="inline-block align-middle">Smazat</span>
-                                        </button>
+                                            <span class="inline-block align-middle">Upravit</span>
+                                        </a>
+                                        <form action="/obor/{{$specialization->id}}/smazat" class="inline ml-4"
+                                              method="post">
+                                            @csrf
+                                            @method("DELETE")
+                                            <button type="submit">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                     stroke="currentColor" class="inline h-5">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                                                </svg>
+                                                <span class="inline-block align-middle">Smazat</span>
+                                            </button>
 
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
+            @endif
 
             <div class="bg-white shadow overflow-hidden sm:rounded-lg mx-5 mt-10">
                 <div class="px-4 py-5 border-b border-gray-200 sm:px-6 flex justify-between">
