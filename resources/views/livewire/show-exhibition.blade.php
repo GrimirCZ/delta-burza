5ebd7a3d201d4cd3a28a46ff1fd6f7dd3a16f816
Matěj Půhoný <info@jetam.eu>
2020-10-05 09:19:07 +0200

    AAAA I HATE CSS MANSON GRID effect

--- resources/views/livewire/show-exhibition.blade.php
+++ resources/views/livewire/show-exhibition.blade.php
@@ -18,83 +18,169 @@
                             nepřihlásil. Buďte první!</h3>
                     </div>
                 @else
-                    <div class="grid md:grid-cols-2 gap-3">
-                        @foreach($registrations as $registration)
-                            <div class="p-5 shadow-sm box-border h-min-content bg-white {{$registration->school->is_school ?  "border-2 border-teal-400" : ""}}">
-                                <div class="leading-3 text-gray-400">
-                                    {{$registration->school->is_school ? "škola" : "firma"}} | okres {{$registration->school->district->name}}
-                                </div>
-                                <a href="/skola/{{$registration->school->id}}">
-                                    <h3 class="text-2xl font-light">
-                                        {{$registration->school->name}}
-                                    </h3>
-                                </a>
+                    <div class="">
+                        <div class="manson-columns">
+                            <div class="col-1">
+                                @foreach($registrations as $registration)
+                                    @if($loop->index%2 == 0)
+                                    <div class="manson-item m-3 p-5 shadow-sm box-border h-min-content bg-white {{$registration->school->is_school ?  "border-2 border-teal-400" : ""}}">
+                                        <div class="leading-3 text-gray-400">
+                                            {{$registration->school->is_school ? "škola" : "firma"}} | okres {{$registration->school->district->name}}
+                                        </div>
+                                        <a href="/skola/{{$registration->school->id}}">
+                                            <h3 class="text-2xl font-light">
+                                                {{$registration->school->name}}
+                                            </h3>
+                                        </a>
 
-                                <table class="table w-full mt-5 text-sm text-gray-600">
-                                    <tbody class="divide-y divide-gray-200">
-                                    @foreach($registration->school->ordered_specializations()->get() as $specialization)
-                                        <tr>
-                                            <td class="py-3">
-                                                <a href="/obor/{{$specialization->id}}">
-                                                    {{$specialization->prescribed_specialization->code}}
-                                                    - {{$specialization->prescribed_specialization->name}} <br/>
-                                                    <i>(ŠVP: {{$specialization->name}})</i>
-                                                </a>
-                                            </td>
-                                            <td class="py-3 text-right">
-                                                <a href="/obor/{{$specialization->id}}"
-                                                   class="btn btn-primary text-sm inline-block">Více
-                                                    informací o oboru</a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
+                                        <table class="table w-full mt-5 text-sm text-gray-600">
+                                            <tbody class="divide-y divide-gray-200">
+                                            @foreach($registration->school->ordered_specializations()->get() as $specialization)
+                                                <tr>
+                                                    <td class="py-3">
+                                                        <a href="/obor/{{$specialization->id}}">
+                                                            {{$specialization->prescribed_specialization->code}}
+                                                            - {{$specialization->prescribed_specialization->name}} <br/>
+                                                            <i>(ŠVP: {{$specialization->name}})</i>
+                                                        </a>
+                                                    </td>
+                                                    <td class="py-3 text-right">
+                                                        <a href="/obor/{{$specialization->id}}"
+                                                           class="btn btn-primary text-sm inline-block">Více
+                                                            informací o oboru</a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                        </table>
 
-                                @if($registration->exhibition->date == current_date_str())
-                                    <a href="/vstoupit/ranni/{{$registration->id}}"
-                                       target="_blank"
-                                       class="btn text-sm text-center btn-primary mt-13 block">
-                                        Připojit se online {{settings("morning_event_start")}}
-                                        - {{settings("morning_event_end")}}
-                                    </a>
-                                    <a href="/vstoupit/vecerni/{{$registration->id}}"
-                                       target="_blank"
-                                       class="btn text-sm text-center btn-primary mt-1 block">
-                                        Připojit se online {{settings("evening_event_start")}}
-                                        - {{settings("evening_event_end")}}
-                                    </a>
-                                @else
-                                    <span
-                                        class="btn text-sm text-center mt-13 block btn-disabled">
+                                        @if($registration->exhibition->date == current_date_str())
+                                            <a href="/vstoupit/ranni/{{$registration->id}}"
+                                               target="_blank"
+                                               class="btn text-sm text-center btn-primary mt-13 block">
+                                                Připojit se online {{settings("morning_event_start")}}
+                                                - {{settings("morning_event_end")}}
+                                            </a>
+                                            <a href="/vstoupit/vecerni/{{$registration->id}}"
+                                               target="_blank"
+                                               class="btn text-sm text-center btn-primary mt-1 block">
+                                                Připojit se online {{settings("evening_event_start")}}
+                                                - {{settings("evening_event_end")}}
+                                            </a>
+                                        @else
+                                            <span
+                                                class="btn text-sm text-center mt-13 block btn-disabled">
                                     Připojit se online {{settings("morning_event_start")}}
                                     - {{settings("morning_event_end")}}
                                 </span>
-                                    <span
-                                        class="btn text-sm text-center mt-1 block btn-disabled">
+                                            <span
+                                                class="btn text-sm text-center mt-1 block btn-disabled">
                                     Připojit se online {{settings("evening_event_start")}}
                                     - {{settings("evening_event_end")}}
                                 </span>
-                                @endif
+                                        @endif
 
-                                <a href="/skola/{{$registration->school->id}}"
-                                   class="btn text-sm text-center mt-1 block bg-teal-400 hover:bg-teal-500 text-white">
-                                    Detail @if($registration->school->is_school) školy @else firmy @endif
-                                </a>
+                                        <a href="/skola/{{$registration->school->id}}"
+                                           class="btn text-sm text-center mt-1 block bg-teal-400 hover:bg-teal-500 text-white">
+                                            Detail @if($registration->school->is_school) školy @else firmy @endif
+                                        </a>
 
-                                <div class="mt-4 text-sm hover:underline text-gray-400">
-                                    <div class="display-inline-block">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                             stroke="currentColor" class="inline-block h-4 align-middle">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                  d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>
-                                        </svg>
-                                        <a href="{{fix_url($registration->school->web)}}" target="_blank"
-                                           class="align-middle">{{$registration->school->web}}</a>
+                                        <div class="mt-4 text-sm hover:underline text-gray-400">
+                                            <div class="display-inline-block">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                     stroke="currentColor" class="inline-block h-4 align-middle">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                          d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>
+                                                </svg>
+                                                <a href="{{fix_url($registration->school->web)}}" target="_blank"
+                                                   class="align-middle">{{$registration->school->web}}</a>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
+                                    @endif
+                                @endforeach
                             </div>
-                        @endforeach
+                            <div class="col-2">
+                                @foreach($registrations as $registration)
+                                    @if($loop->index%2 == 1)
+                                    <div class="manson-item m-3 p-5 shadow-sm box-border h-min-content bg-white {{$registration->school->is_school ?  "border-2 border-teal-400" : ""}}">
+                                        <div class="leading-3 text-gray-400">
+                                            {{$registration->school->is_school ? "škola" : "firma"}} | okres {{$registration->school->district->name}}
+                                        </div>
+                                        <a href="/skola/{{$registration->school->id}}">
+                                            <h3 class="text-2xl font-light">
+                                                {{$registration->school->name}}
+                                            </h3>
+                                        </a>
+
+                                        <table class="table w-full mt-5 text-sm text-gray-600">
+                                            <tbody class="divide-y divide-gray-200">
+                                            @foreach($registration->school->ordered_specializations()->get() as $specialization)
+                                                <tr>
+                                                    <td class="py-3">
+                                                        <a href="/obor/{{$specialization->id}}">
+                                                            {{$specialization->prescribed_specialization->code}}
+                                                            - {{$specialization->prescribed_specialization->name}} <br/>
+                                                            <i>(ŠVP: {{$specialization->name}})</i>
+                                                        </a>
+                                                    </td>
+                                                    <td class="py-3 text-right">
+                                                        <a href="/obor/{{$specialization->id}}"
+                                                           class="btn btn-primary text-sm inline-block">Více
+                                                            informací o oboru</a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                        </table>
+
+                                        @if($registration->exhibition->date == current_date_str())
+                                            <a href="/vstoupit/ranni/{{$registration->id}}"
+                                               target="_blank"
+                                               class="btn text-sm text-center btn-primary mt-13 block">
+                                                Připojit se online {{settings("morning_event_start")}}
+                                                - {{settings("morning_event_end")}}
+                                            </a>
+                                            <a href="/vstoupit/vecerni/{{$registration->id}}"
+                                               target="_blank"
+                                               class="btn text-sm text-center btn-primary mt-1 block">
+                                                Připojit se online {{settings("evening_event_start")}}
+                                                - {{settings("evening_event_end")}}
+                                            </a>
+                                        @else
+                                            <span
+                                                class="btn text-sm text-center mt-13 block btn-disabled">
+                                    Připojit se online {{settings("morning_event_start")}}
+                                    - {{settings("morning_event_end")}}
+                                </span>
+                                            <span
+                                                class="btn text-sm text-center mt-1 block btn-disabled">
+                                    Připojit se online {{settings("evening_event_start")}}
+                                    - {{settings("evening_event_end")}}
+                                </span>
+                                        @endif
+
+                                        <a href="/skola/{{$registration->school->id}}"
+                                           class="btn text-sm text-center mt-1 block bg-teal-400 hover:bg-teal-500 text-white">
+                                            Detail @if($registration->school->is_school) školy @else firmy @endif
+                                        </a>
+
+                                        <div class="mt-4 text-sm hover:underline text-gray-400">
+                                            <div class="display-inline-block">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                     stroke="currentColor" class="inline-block h-4 align-middle">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                          d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>
+                                                </svg>
+                                                <a href="{{fix_url($registration->school->web)}}" target="_blank"
+                                                   class="align-middle">{{$registration->school->web}}</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    @endif
+                                @endforeach
+                            </div>
+                        </div>
                     </div>
                 @endif
             </div>
