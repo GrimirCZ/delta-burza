7bd12ea9266a482a542a87dda63df40b909a3be8
grimir <falta.vit@zsbcuo.cz>
2020-10-08 18:40:21 +0200

    add ability to regenerate invoices

---
+++ app/Console/Commands/RegenerateInvoice.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\SendInvoice;
+use App\Models\Order;
+use Illuminate\Console\Command;
+
+class RegenerateInvoice extends Command
+{
+    protected $signature = 'regenerate:invoice {order_id}';
+
+    protected $description = 'Generate and send a new invoice for the order identifierd by {order_id}';
+
+    public function handle()
+    {
+        $order = Order::findOrFail($this->argument("order_id"));
+
+        $this->info("Sending new invoice...");
+
+        SendInvoice::dispatch($order->id);
+
+        $this->info("Sene");
+        //
+    }
+}
