d6bdfd734ce81885f86e0a3d77504c75a0fb7556
grimir <falta.vit@zsbcuo.cz>
2020-10-10 11:42:52 +0200

    Add admin dashboard

---
+++ resources/views/livewire/admin-dashboard.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <x-own-header>
+        Správcovské rozhraní
+    </x-own-header>
+
+    <div class="max-w-7xl mx-auto">
+        <x-dashboard-card>
+            <a href="{{url("/clanky")}}" class="btn btn-primary">Články</a>
+        </x-dashboard-card>
+    </div>
+
+</div>
