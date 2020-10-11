4b6506fceb5c95605eab05fb623ae9ad86231462
grimir <falta.vit@zsbcuo.cz>
2020-10-11 11:59:17 +0200

    fix no image upload

--- app/Http/Livewire/AdminArticleCreate.php
+++ app/Http/Livewire/AdminArticleCreate.php
@@ -29,16 +29,20 @@ class AdminArticleCreate extends Component
     {
         $this->validate();
 
-        $img = Image::make($this->cover)
-            ->resize(800, null, function($constraint){
-                $constraint->aspectRatio();
-            });
+        if($this->cover){
+            $img = Image::make($this->cover)
+                ->resize(800, null, function($constraint){
+                    $constraint->aspectRatio();
+                });
 
-        $filepath = "images/" . uniqid() . ".jpg";
+            $filepath = "images/" . uniqid() . ".jpg";
 
-        $s3 = Storage::disk("s3");
-        $s3->put($filepath, $img->stream('jpg', 100), 'public');
-        $url = $s3->url($filepath);
+            $s3 = Storage::disk("s3");
+            $s3->put($filepath, $img->stream('jpg', 100), 'public');
+            $url = $s3->url($filepath);
+        }else{
+            $url = null;
+        }
 
         Article::create([
             'title' => $this->title,
