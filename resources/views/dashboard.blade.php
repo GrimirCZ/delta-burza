2676681be9fa266d71f4b280143ad00787892333
grimir <falta.vit@zsbcuo.cz>
2020-10-12 10:56:00 +0200

    quick fix dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -173,7 +173,8 @@
                                             <button type="submit">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                      stroke="currentColor" class="inline h-5">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                          stroke-width="2"
                                                           d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                                                 </svg>
                                                 <span class="inline-block align-middle">Smazat</span>
@@ -239,14 +240,16 @@
                                         </a>
                                     </td>
                                     <td class="px-8 py-5 text-right">
-                                        <form action="/spolecnost/skola/{{$related_school->id}}/smazat" class="inline ml-4"
+                                        <form action="/spolecnost/skola/{{$related_school->id}}/smazat"
+                                              class="inline ml-4"
                                               method="post">
                                             @csrf
                                             @method("DELETE")
                                             <button type="submit">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                      stroke="currentColor" class="inline h-5">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                          stroke-width="2"
                                                           d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                                                 </svg>
                                                 <span class="inline-block align-middle">Smazat</span>
@@ -287,8 +290,11 @@
                                     </a>
                                 </td>
                                 <td class="px-8 py-5">
-                                    @if(!$registration->order_registration->fulfilled_at != null)
-                                        <span class="text-red-700 font-semibold">Nezaplaceno</span>
+{{--                                    TODO: examin--}}
+                                    @if($registration->order_registration != null)
+                                        @if(!$registration->order_registration->fulfilled_at != null)
+                                            <span class="text-red-700 font-semibold">Nezaplaceno</span>
+                                        @endif
                                     @endif
                                 </td>
                                 <td class="px-8 py-5 text-right">
