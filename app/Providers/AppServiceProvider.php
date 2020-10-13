49917e432d5ec4c844399614ec786ce4f73050ef
grimir <falta.vit@zsbcuo.cz>
2020-10-13 22:36:58 +0200

    force https on links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App;
 use App\Settings;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(Settings::class, function(){
             return Settings::make(storage_path('app/settings.json'));
         });
+
+        if(config('app.env') === 'production'){
+            URL::forceScheme('https');
+        }
         //
     }
 
