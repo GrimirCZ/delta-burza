ddc307b16b0ff4f24f59b2e9e2f57bc0ef04e39d
grimir <falta.vit@zsbcuo.cz>
2020-10-24 10:43:36 +0200

    update broadcasting config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
+                'host' => env("PUSHER_APP_HOST", '127.0.0.1'),
+                'port' => env("PUSHER_APP_PORT", 6001),
                 'useTLS' => true,
+                'scheme' => 'http'
             ],
         ],
 
