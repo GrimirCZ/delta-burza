9f285eab1a08d6febf17a2edd51ba995c4b75d1d
grimir <falta.vit@zsbcuo.cz>
2020-10-30 13:58:20 +0100

    send mail when school is contacted

---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Order;
+use App\Models\SchoolContact;
+use App\Models\User;
+use Doctrine\DBAL\Connection;
+use Illuminate\Mail\Mailable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    protected SchoolContact $contact;
+
+    public function __construct(SchoolContact $contact)
+    {
+        $this->contact = $contact;
+        //
+    }
+
+    public function build()
+    {
+        return $this->from("info@burzaskol.online")
+            ->subject("Zálohová faktura k objednávce č. " . $this->order->id)
+            ->view('emails.contact')
+            ->with([
+                'contact' => $this->contact,
+            ]);
+    }
+}
---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,31 @@
+<h1>Někdo má zájem o vaši školu.</h1>
+
+<p>
+    @if(isset($contact->name))
+        Jméno: <span id="name">{{$contact->name}}</span><br/>
+    @endif
+    @if(isset($contact->email))
+        Email: <span id="email">{{$contact->email}}</span><br/>
+    @endif
+    @if(isset($contact->phone))
+        Telefon: <span id="phone">{{$contact->phone}}</span><br/>
+    @endif
+    @if(isset($contact->registration_id))
+        Výstava: <span>{{$contact->registration->exhibition->name}}</span>
+    @endif
+</p>
+
+@if(isset($contact->body))
+    <p>
+        Zpráva:<br/>
+        <span class="message">{{$contact->body}}</span>
+    </p>
+@endif
+
+<p>
+    DELTA - Střední škola informatiky a ekonomie, s.r.o.,<br/>
+    Provozovatel portálu: BurzaŠkol.Online<br/>
+    Ke Kamenci 151, 530 03 Pardubice<br/>
+    <a href="https://www.delta-skola.cz">www.delta-skola.cz</a>, <a href="mailto:info@delta-skola.cz">info@delta-skola.cz</a>,
+    <a href="tel:466 611 106">466 611 106</a>
+</p>
