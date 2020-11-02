541d7d4aa1ec0c871904ce6430ffe2c01f62031e
grimir <falta.vit@zsbcuo.cz>
2020-11-02 18:18:45 +0100

    improve chat

--- resources/views/livewire/visitor-chat.blade.php
+++ resources/views/livewire/visitor-chat.blade.php
@@ -1,11 +1,13 @@
 <div>
-    <x-own-header>
-        Chat s {{$registration->school->name}}
-    </x-own-header>
+    <div class="hidden md:block">
+        <x-own-header>
+            Chat s {{$registration->school->name}}
+        </x-own-header>
+    </div>
 
     <div class="max-w-7xl mx-auto md:pt-5 pb-10 sm:px-6 lg:px-8 w-100">
         <div class="bg-white shadow-sm text-center p-5 mx-5">
-            <div class="px-4 py-5 border-b border-gray-200 mb-4 sm:px-6 flex justify-between hidden md:block">
+            <div class="px-4 py-5 border-b border-gray-200 mb-4 sm:px-6 flex justify-between">
                 <h3 class="text-2xl font-bold">Chat</h3>
                 <div id="currently_responding_to">
                     @if($currently_responding_to > 0)
