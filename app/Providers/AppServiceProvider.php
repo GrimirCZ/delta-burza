7bbba02759fc3e961fea7e9bdbfdc4035234edb0
grimir <falta.vit@zsbcuo.cz>
2020-11-02 12:18:52 +0100

    fix encoding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use App;
 use App\Settings;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use URL;
@@ -36,7 +35,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         App::setLocale('cs');
-        Blade::setEchoFormat('e(utf8_encode(%s))');
         Schema::defaultStringLength(191);
         //
     }
