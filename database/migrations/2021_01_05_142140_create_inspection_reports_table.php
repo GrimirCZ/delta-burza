722eae2c34ebdfca1cf173997c21fa90b6f0deea
grimir <falta.vit@zsbcuo.cz>
2021-01-05 14:26:01 +0100

    add ico, start_date and end_date to composite unique index

--- database/migrations/2021_01_05_142140_create_inspection_reports_table.php
+++ database/migrations/2021_01_05_142140_create_inspection_reports_table.php
@@ -12,9 +12,13 @@ class CreateInspectionReportsTable extends Migration
             $table->bigIncrements('id');
 
             $table->string("ico");
+
             $table->date("start_date");
             $table->date("end_date");
+
             $table->string("url", 255);
+
+            $table->unique(['ico', 'start_date', 'end_date']);
 
             $table->foreignId("school_id")->constrained();
             //
