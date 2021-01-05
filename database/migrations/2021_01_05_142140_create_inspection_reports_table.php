7c949133759913733c2d7146356475ac6a0083cb
grimir <falta.vit@zsbcuo.cz>
2021-01-05 14:23:14 +0100

    create inspection_reports table

---
+++ database/migrations/2021_01_05_142140_create_inspection_reports_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInspectionReportsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('inspection_reports', function(Blueprint $table){
+            $table->bigIncrements('id');
+
+            $table->string("ico");
+            $table->date("start_date");
+            $table->date("end_date");
+            $table->string("url", 255);
+
+            $table->foreignId("school_id")->constrained();
+            //
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('inspection_reports');
+    }
+}
