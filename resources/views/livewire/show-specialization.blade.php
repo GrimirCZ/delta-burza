a497e25b0d906be88486a8abd8fe30c1290cee05
grimir <falta.vit@zsbcuo.cz>
2021-01-12 15:47:20 +0100

    show similar prescribed_specializations

--- resources/views/livewire/show-specialization.blade.php
+++ resources/views/livewire/show-specialization.blade.php
@@ -37,7 +37,7 @@
                     <div class="flex justify-content-start mmb-3">
                         <div class="relative pr-8 py-4 pb-0">
                             <div class="text-2xl">
-                                Výsledky státních maturit za skupinu oborů {{$spec_group->code}}
+                                Výsledky státních maturit za skupinu oborů {{$spec_group->code}} - {{$spec_group->name}}
                             </div>
                             <div class="livewire-tooltip-in-title">
                                 <livewire:tooltip :title='"Výsledky státních maturit za skupinu oborů $spec_group->code"' :content="$textSimiliarObory"/>
