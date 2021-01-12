8374e1123e58ec9951173dce662d8546929a0fb3
grimir <falta.vit@zsbcuo.cz>
2021-01-12 16:06:22 +0100

    show similar prescribed_specializations

--- app/Http/Livewire/ShowSpecialization.php
+++ app/Http/Livewire/ShowSpecialization.php
@@ -48,8 +48,8 @@ class ShowSpecialization extends Component
 
     private function generateSimilarOboryText(?SpecializationGroup $spec_group) : string
     {
-        $res = "<div>CERMAT u škol bohužel nezobrazuje výsledky po jednotlivých oborech ale pouze po SKUPINÁCH oborů. U každého oboru školy se proto zobrazují výsledky, kterých škola dosáhla v rámci celé skupiny.</div>"
-            . "<div class='mt-6'>Do skupiny oborů $spec_group->code - $spec_group->name spadají tyto obory:</div><ul class='mt-2'>";
+        $res = "<div class='text-sm text-gray-500 font-normal'>CERMAT u škol bohužel nezobrazuje výsledky po jednotlivých oborech ale pouze po SKUPINÁCH oborů. U každého oboru školy se proto zobrazují výsledky, kterých škola dosáhla v rámci celé skupiny.</div>"
+            . "<div class='mt-6 text-sm text-gray-500 font-normal'>Do skupiny oborů $spec_group->code - $spec_group->name spadají tyto obory:</div><ul class='mt-2'>";
 
         foreach($spec_group->prescribed_specializations as $ps){
             $res .= "<li class='text-sm text-gray-500 font-normal'>$ps->code - $ps->name</li>";
