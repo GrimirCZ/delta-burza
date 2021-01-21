f4579b7b2e11b98fd1639d30328723febfb7f8c7
grimir <falta.vit@zsbcuo.cz>
2021-01-21 17:38:23 +0100

    competition results

--- resources/views/livewire/tooltip.blade.php
+++ resources/views/livewire/tooltip.blade.php
@@ -4,7 +4,7 @@
     ><img src="{{url("images/questionmark.png")}}" alt="question mark">
     </span>
     @if($show)
-        <div class="fixed z-10 inset-0 overflow-y-auto">
+        <div class="fixed z-20 inset-0 overflow-y-auto">
             <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <div class="fixed inset-0 transition-opacity" aria-hidden="true">
                     <div class="absolute inset-0 bg-black opacity-75"></div>
