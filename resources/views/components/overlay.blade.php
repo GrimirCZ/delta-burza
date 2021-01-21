6f83779c2a39eeeec409f1ebc0ab5c552f82560a
grimir <falta.vit@zsbcuo.cz>
2021-01-21 18:31:57 +0100

    competition results

--- resources/views/components/overlay.blade.php
+++ resources/views/components/overlay.blade.php
@@ -16,7 +16,7 @@
                             {{$title}}
                         </h3>
                         <div class="mt-2">
-                            <div class="text-sm text-gray-500 font-normal max-h-full sm:max-h-3/4 overflow-auto">
+                            <div class="max-h-full sm:max-h-3/4 overflow-auto">
                                 {!! $slot !!}
                             </div>
                         </div>
